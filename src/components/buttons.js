bbc33d92851b6492d8bf6c358dfa808ef5385066
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2021-06-23 15:48:18 +0530

    still have some bugs to fix

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -21,7 +21,9 @@ export class Buttons extends Component {
     answer(){
         switch(this.state.func){
             case '+':
-                this.setState({result: this.state.num2 + this.state.num1})
+                this.setState({result: this.state.num2 + this.state.num1},() => console.log("result = " + this.state.result))
+                console.log(this.state.num2 + " + " + this.state.num1)
+
             case '-':
                 this.setState({result: this.state.num2 - this.state.num1})
             case '*':
